c0806b3aa6b7ce57bcabad494250a8f7f602f1d0
TJ Viaud <tjviaud@gmail.com>
2025-12-04 01:51:25 +0000

    saving

---
+++ src/SecondsCounter.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function SecondsCounter () {
+    
+    return (
+        <>
+            <h1>SecondsCounter</h1>
+        </>
+    )
+}
+
+export default SecondsCounter
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,33 @@
 import React from "react";
+import SecondsCounter from "./SecondsCounter";
+import ReactDOM from 'react-dom';
+import Card from './Card.jsx'
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(<App />);
 
-//create your first component
-const Home = () => {
+
+function SecondsCounter() {
 	return (
-		<div className="text-center">
-            
+		<>
+		  <SecondsCounter />
+		</>
+	)
+	function Card (props){
+    
+		return (
+        <div className="card">
+            <img className="card-img-top" src={props.imageUrl} alt="Card image cap" />
+            <div className="card-body">
+                <h5 className="card-title">{props.title}</h5>
+                <p className="card-text">Some quick example text to build on the card title and fill the card's content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    );
+}
+}
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+   
 
-export default Home;
+export default SecondsCounter
